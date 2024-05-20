caf4aba694729933b75917b42460144e137c692d
seaskyho <seaskyho@gmail.com>
2024-05-20 17:49:19 +1000

    update

--- runlocal_eval.sh
+++ runlocal_eval.sh
@@ -3,7 +3,7 @@ SEED=10
 python test.py \
   --dataset visa \
   --data_path data/visa \
-  --save_path ./results/visa/0shot_baseline_$SEED \
+  --save_path ./results/visa/0shot_baseline_nodice_$SEED \
   --features_list 6 12 18 24 \
   --image_size 518 \
   --depth 9 \
